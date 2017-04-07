473e5c0f7d45261993e63503346f81e8fb32b2f3
Alex Soares <alexhbs@gmail.com>
2017-04-06 21:36:18 -0300

    added function logic and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix() will receive matrix x and return a list of functions:
+## get & set: get and set internal matrix value to be used by cacheSolve()
+## getinverse & setinverse: gets or sets the received inverse of "x"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() will test if the inverse of matrix "x" has been calculated and
+## (it should be cached within object "x" in the scope of makeCacheMatrix() if so)
+## if not, calculates the inverse of such matrix, returns it and then caches it
+## for faster access
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
+
